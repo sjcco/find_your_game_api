17924306d498b786801359a16ee8d21f6be64531
CarlosC <sjcco@gmail.com>
2021-09-01 20:17:31 -0500

    Add models and their relations

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,6 @@
 class Game < ApplicationRecord
+  validates :title, presence: true, uniqueness: true
+  validates :year, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1960, less_than: 2050 }
+
+  has_and_belongs_to_many :categories
 end
